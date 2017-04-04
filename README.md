2e1c4183481c5b4eeb855a01ff2e0136596912c5
Brian Guilder <bguilder@gmail.com>
2017-04-04 01:07:53 -0400

    adds basic get and post pages

--- README.md
+++ README.md
@@ -5,6 +5,6 @@
 1. Clone the repo (https://help.github.com/articles/cloning-a-repository/)
 2. Install NodeJS & npm (https://docs.npmjs.com/getting-started/installing-node)
 3. Install Nodemon ```npm install -g nodemon```
-4. cd into your working directory 
+4. cd into your working directory
 5. Install dependencies (npm install)
 6. Start the server 👍 ```nodemon app.js```
--- app.js
+++ app.js
@@ -3,18 +3,13 @@ var app = express();
 var config = require("./config");
 var mongoose = require("mongoose");
 var controller = require('./controller');
+var Fuel = require("./models/fuelModel")
 
 app.set('port', (process.env.PORT || 3000));
 
 app.set('view engine', 'ejs');
 
 app.use('/public', express.static(__dirname + '/public'));
-
-app.get('/',function(req,res){
-    res.render('login');
-})
-
-
 
 mongoose.connect(config.getDbConnectionString(),function(err){
     if(err){
